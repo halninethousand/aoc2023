ec9b96e0d958aa3a45435e24dc540d7e1a6114bc
unknown <tsvetkovprelude@gmail.com>
2024-10-01 18:20:25 +0300

    day5 part1 rewrite

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,85 +1,32 @@
-use std::collections::HashMap;
-
 fn main() {
-    let input: Vec<String> = include_str!("../../data/day5.txt")
-        .lines()
-        .map(|x| x.to_string())
-        .collect();
-    
-    let seeds: Vec<u64> = input[0]
-        .split_whitespace()
-        .skip(1)
-        .filter_map(|s| s.parse().ok())
-        .collect();
-    
-    let mut current_map = Vec::new();
-    let mut maps = Vec::new();
+    let data: String = include_str!("../../data/day5.txt").to_owned();
+    let sections = parse_data(&data);
+    let part1 = part1(&sections);
 
-    for line in &input[1..] {
-        if line.is_empty() {
-            if !current_map.is_empty() {
-                maps.push(current_map);
-                current_map = Vec::new();
-            }
-        } else {
-            current_map.push(line);
-        }
-    }
-
-    if !current_map.is_empty() {
-        maps.push(current_map);
-    }
-
-    println!("Seeds: {:?}", seeds);
- 
-    let processed_maps: Vec<HashMap<u64, u64>> = maps.iter().map(|map| {
-        let map_name = map[0].trim_end_matches(" map:");
-        let mappings: Vec<(u64, u64, u64)> = map[1..]
-            .iter()
-            .map(|line| {
-                let nums: Vec<u64> = line
-                    .split_whitespace()
-                    .map(|s| s.parse().unwrap())
-                    .collect();
-                (nums[0], nums[1], nums[2])
-            })
-            .collect();
-        
-        let mut hmap: HashMap<u64, u64> = HashMap::new();
-        
-        for &(dest_start, source_start, length) in &mappings {
-            for i in 0..length {
-                hmap.insert(source_start + i, dest_start + i);
-            }
-        }
-
-        fill_default_mappings(&mut hmap);
-        
-        println!("Map: {}", map_name);
-        println!("Mappings: {:?}", mappings);
-        
-        hmap
-
-    }).collect();
-
-    let final_locations: Vec<u64> = seeds.iter().map(|&seed| {
-        processed_maps.iter().fold(seed, |value, map| {
-            *map.get(&value).unwrap_or(&value)
-        })
-    }).collect();
-
-    println!("Final locations: {:?}", final_locations);
-    
-    if let Some(&lowest_location) = final_locations.iter().min() {
-        println!("Lowest location number: {}", lowest_location);
-    } else {
-        println!("No valid locations found.");
-    }
+    println!("Part1 lowest location seed: {part1}");
 }
 
-fn fill_default_mappings(map: &mut HashMap<u64, u64>) {
-    let max_key = *map.keys().max().unwrap_or(&0);
-    for i in 0..=max_key {
-        map.entry(i).or_insert(i);
+fn parse_data(data: &str) -> Vec<Vec<usize>> {
+
+    let ret = data.split("\r\n\r\n")
+        .map(|x| x.split_once(':').unwrap().1.replace('\n', " ").split_whitespace()
+            .map(|z| z.parse::<usize>().unwrap()).collect::<Vec<_>>()).collect();
+    ret
+}
+
+fn part1(sections: &Vec<Vec<usize>>) -> usize {
+    let mut seeds = sections[0].clone().to_owned();
+    for section in sections[1..].iter() {
+        'seed: for seed in seeds.iter_mut() {
+            for chunk in section.chunks(3) {
+                if let &[dest_start, source_start, map_range] = chunk {
+                    if (source_start <= *seed) && ((source_start + map_range) >= *seed) {
+                        *seed = dest_start + *seed - source_start;
+                        continue 'seed
+                    }
+                }
+            }
+        }
     }
+    *seeds.iter().min().unwrap()
 }
